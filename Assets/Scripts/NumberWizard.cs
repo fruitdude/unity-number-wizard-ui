cd07a4a4e4c8e350c336510519371bd5ede0ade0
fruitdude <stevenrogge95@gmail.com>
2021-02-09 11:49:19 +0100

    added files

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour{
+    [SerializeField] int maximum_number;
+    [SerializeField] int minimum_number;
+    [SerializeField] TextMeshProUGUI guess_number_text;
+    int guessed_number;
+    
+    void Start() {
+        StartInstructions();
+    }
+
+    void StartInstructions() {
+        NextGuess();
+    }
+
+
+    void NextGuess() {
+        guessed_number = Random.Range(minimum_number, maximum_number + 1);
+        guess_number_text.text = guessed_number.ToString();
+    }
+
+    public void OnPressHigher() {
+        minimum_number = guessed_number + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        maximum_number = guessed_number - 1;
+        NextGuess();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+    public void LoadNextScene() {
+        //gets the index of the current running scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //adds plus 1 to the current scene index
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void loadStartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void loadGameplayScene() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitApplication() {
+        Application.Quit();
+    }
+}
